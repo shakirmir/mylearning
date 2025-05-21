a8aecd8b06db8006e516ea3e529c2c70dca018a6
sonik <azuredemo95@gmail.com>
2025-05-22 03:12:38 +0530

    added files

---
+++ providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "b2f1e96a-2500-4557-9658-7dea028c06a4"
+
+
+
+}
---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "client_secret" {
+}
+variable "subscription_id" {
+    default = "b2f1e96a-2500-4557-9658-7dea028c06a4"
+
+}
+variable "client_id" {
+
+}
+
+variable "tenant_id" {
+
+}
+
+variable "resource_group_name"{
+    type = string
+    default = "rgtf"
+
+}
+variable "resource_group_location"{
+    
+    default = "eastus"
+    
+}
